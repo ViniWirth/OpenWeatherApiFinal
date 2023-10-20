a882f45ff8499b245613748fd0d66b814bca51ae
ViniWirth <138532333+ViniWirth@users.noreply.github.com>
2023-10-20 08:19:51 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+const apiKey = "951a31a0e6ada773f76ccc12d7c61ae2";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    return data;
+};
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+      );
+}
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+});
